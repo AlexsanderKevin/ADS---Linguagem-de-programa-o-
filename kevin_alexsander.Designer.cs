f0b3cfac36fe8ea6c9fb8ae27eff50ae15689af9
ChesterKing66 <kevin13348@gmail.com>
2021-10-02 10:58:33 -0300

    first commit

---
+++ kevin_alexsander.Designer.cs
@@ -0,0 +1,199 @@
+﻿
+namespace Prova_01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.num1Label = new System.Windows.Forms.Label();
+            this.num2Label = new System.Windows.Forms.Label();
+            this.textNum1 = new System.Windows.Forms.TextBox();
+            this.textNum2 = new System.Windows.Forms.TextBox();
+            this.labelOperation = new System.Windows.Forms.Label();
+            this.radioAdd = new System.Windows.Forms.RadioButton();
+            this.radioSub = new System.Windows.Forms.RadioButton();
+            this.radioMulti = new System.Windows.Forms.RadioButton();
+            this.radioDiv = new System.Windows.Forms.RadioButton();
+            this.textResult = new System.Windows.Forms.TextBox();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // num1Label
+            // 
+            this.num1Label.AutoSize = true;
+            this.num1Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.num1Label.Location = new System.Drawing.Point(49, 50);
+            this.num1Label.Name = "num1Label";
+            this.num1Label.Size = new System.Drawing.Size(99, 24);
+            this.num1Label.TabIndex = 1;
+            this.num1Label.Text = "Número 1:";
+            // 
+            // num2Label
+            // 
+            this.num2Label.AutoSize = true;
+            this.num2Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.num2Label.Location = new System.Drawing.Point(49, 102);
+            this.num2Label.Name = "num2Label";
+            this.num2Label.Size = new System.Drawing.Size(99, 24);
+            this.num2Label.TabIndex = 2;
+            this.num2Label.Text = "Número 2:";
+            // 
+            // textNum1
+            // 
+            this.textNum1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textNum1.Location = new System.Drawing.Point(195, 45);
+            this.textNum1.Name = "textNum1";
+            this.textNum1.Size = new System.Drawing.Size(153, 29);
+            this.textNum1.TabIndex = 3;
+            // 
+            // textNum2
+            // 
+            this.textNum2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textNum2.Location = new System.Drawing.Point(195, 97);
+            this.textNum2.Name = "textNum2";
+            this.textNum2.Size = new System.Drawing.Size(153, 29);
+            this.textNum2.TabIndex = 4;
+            // 
+            // labelOperation
+            // 
+            this.labelOperation.AutoSize = true;
+            this.labelOperation.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelOperation.Location = new System.Drawing.Point(49, 158);
+            this.labelOperation.Name = "labelOperation";
+            this.labelOperation.Size = new System.Drawing.Size(265, 24);
+            this.labelOperation.TabIndex = 5;
+            this.labelOperation.Text = "Escolha a operação desejada:";
+            // 
+            // radioAdd
+            // 
+            this.radioAdd.AutoSize = true;
+            this.radioAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioAdd.Location = new System.Drawing.Point(334, 158);
+            this.radioAdd.Name = "radioAdd";
+            this.radioAdd.Size = new System.Drawing.Size(87, 28);
+            this.radioAdd.TabIndex = 7;
+            this.radioAdd.TabStop = true;
+            this.radioAdd.Text = "Adição";
+            this.radioAdd.UseVisualStyleBackColor = true;
+            this.radioAdd.CheckedChanged += new System.EventHandler(this.radioAdd_CheckedChanged);
+            // 
+            // radioSub
+            // 
+            this.radioSub.AutoSize = true;
+            this.radioSub.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioSub.Location = new System.Drawing.Point(334, 192);
+            this.radioSub.Name = "radioSub";
+            this.radioSub.Size = new System.Drawing.Size(113, 28);
+            this.radioSub.TabIndex = 8;
+            this.radioSub.TabStop = true;
+            this.radioSub.Text = "Subtração";
+            this.radioSub.UseVisualStyleBackColor = true;
+            this.radioSub.CheckedChanged += new System.EventHandler(this.radioSub_CheckedChanged);
+            // 
+            // radioMulti
+            // 
+            this.radioMulti.AutoSize = true;
+            this.radioMulti.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioMulti.Location = new System.Drawing.Point(334, 226);
+            this.radioMulti.Name = "radioMulti";
+            this.radioMulti.Size = new System.Drawing.Size(137, 28);
+            this.radioMulti.TabIndex = 9;
+            this.radioMulti.TabStop = true;
+            this.radioMulti.Text = "Multiplicação";
+            this.radioMulti.UseVisualStyleBackColor = true;
+            this.radioMulti.CheckedChanged += new System.EventHandler(this.radioMulti_CheckedChanged);
+            // 
+            // radioDiv
+            // 
+            this.radioDiv.AutoSize = true;
+            this.radioDiv.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioDiv.Location = new System.Drawing.Point(334, 260);
+            this.radioDiv.Name = "radioDiv";
+            this.radioDiv.Size = new System.Drawing.Size(88, 28);
+            this.radioDiv.TabIndex = 10;
+            this.radioDiv.TabStop = true;
+            this.radioDiv.Text = "Divisão";
+            this.radioDiv.UseVisualStyleBackColor = true;
+            this.radioDiv.CheckedChanged += new System.EventHandler(this.radioDiv_CheckedChanged);
+            // 
+            // textResult
+            // 
+            this.textResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textResult.Location = new System.Drawing.Point(195, 328);
+            this.textResult.Name = "textResult";
+            this.textResult.Size = new System.Drawing.Size(153, 29);
+            this.textResult.TabIndex = 12;
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelResult.Location = new System.Drawing.Point(49, 333);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(99, 24);
+            this.labelResult.TabIndex = 11;
+            this.labelResult.Text = "Resultado:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 396);
+            this.Controls.Add(this.textResult);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.radioDiv);
+            this.Controls.Add(this.radioMulti);
+            this.Controls.Add(this.radioSub);
+            this.Controls.Add(this.radioAdd);
+            this.Controls.Add(this.labelOperation);
+            this.Controls.Add(this.textNum2);
+            this.Controls.Add(this.textNum1);
+            this.Controls.Add(this.num2Label);
+            this.Controls.Add(this.num1Label);
+            this.Name = "Form1";
+            this.Text = "Prova 01";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label num1Label;
+        private System.Windows.Forms.Label num2Label;
+        private System.Windows.Forms.TextBox textNum1;
+        private System.Windows.Forms.TextBox textNum2;
+        private System.Windows.Forms.Label labelOperation;
+        private System.Windows.Forms.RadioButton radioAdd;
+        private System.Windows.Forms.RadioButton radioSub;
+        private System.Windows.Forms.RadioButton radioMulti;
+        private System.Windows.Forms.RadioButton radioDiv;
+        private System.Windows.Forms.TextBox textResult;
+        private System.Windows.Forms.Label labelResult;
+    }
+}
+
---
+++ kevin_alexsander.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prova_01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void num1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioAdd_CheckedChanged(object sender, EventArgs e)
+        {
+            int num1 = Convert.ToInt32(textNum1.Text);
+            int num2 = Convert.ToInt32(textNum2.Text);
+
+            textResult.Text = Convert.ToString(num1 + num2);
+        }
+
+        private void radioSub_CheckedChanged(object sender, EventArgs e)
+        {
+            float num1 = Convert.ToInt32(textNum1.Text);
+            int num2 = Convert.ToInt32(textNum2.Text);
+
+            textResult.Text = Convert.ToString(num1 - num2);
+        }
+
+        private void radioMulti_CheckedChanged(object sender, EventArgs e)
+        {
+            float num1 = Convert.ToInt32(textNum1.Text);
+            int num2 = Convert.ToInt32(textNum2.Text);
+
+            textResult.Text = Convert.ToString(num1 * num2);
+        }
+
+        private void radioDiv_CheckedChanged(object sender, EventArgs e)
+        {
+            float num1 = Convert.ToInt32(textNum1.Text);
+            int num2 = Convert.ToInt32(textNum2.Text);
+
+            textResult.Text = Convert.ToString(num1 / num2);
+        }
+    }
+}
